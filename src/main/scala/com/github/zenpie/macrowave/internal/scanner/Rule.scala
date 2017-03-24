824fa6ef0b73da0627000153dbef70e35dd53ed6
kuchenkruste <me@kuchenkruste.com>
2017-03-24 15:00:19 +0100

    Added show function for scanner rules

--- src/main/scala/com/github/zenpie/macrowave/internal/scanner/Rule.scala
+++ src/main/scala/com/github/zenpie/macrowave/internal/scanner/Rule.scala
@@ -1,9 +1,32 @@
 package com.github.zenpie.macrowave.internal.scanner
 
-sealed trait Rule
+sealed trait Rule {
+  def show: String
+}
 
-case class Concatenate(left: Rule, right: Rule) extends Rule
-case class Alternate(left: Rule, right: Rule) extends Rule
-case class Range(from: Char, to: Char) extends Rule
-case class Kleene(rule: Rule) extends Rule
-case class Optional(rule: Rule) extends Rule
+case class Concatenate(left: Rule, right: Rule) extends Rule {
+  def show = (left, right) match {
+    case (_: Alternate, _: Alternate) => s"(${left.show})(${right.show})"
+    case (_: Alternate, _)            => s"(${left.show})${right.show}"
+    case (_, _: Alternate)            => s"${left.show}(${right.show})"
+    case _                            => s"${left.show}${right.show}"
+  }
+}
+case class Alternate(left: Rule, right: Rule) extends Rule {
+  def show = left.show + "|" + right.show
+}
+case class Range(from: Char, to: Char) extends Rule {
+  def show = if (from == to) from + "" else s"[$from-$to]"
+}
+case class Kleene(rule: Rule) extends Rule {
+  def show = rule match {
+    case _: Concatenate | _: Alternate => s"(${rule.show})*"
+    case _ => rule.show + "*"
+  }
+}
+case class Optional(rule: Rule) extends Rule {
+  def show = rule match {
+    case _: Concatenate | _: Alternate => s"(${rule.show})?"
+    case _ => rule.show + "?"
+  }
+}

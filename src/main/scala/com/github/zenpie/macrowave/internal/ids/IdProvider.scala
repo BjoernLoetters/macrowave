de9b810380cc6a3cdc37ff5ee71f1b6318dc5e41
kuchenkruste <me@kuchenkruste.com>
2017-03-27 13:50:09 +0200

    Introduced data tables for ids

--- src/main/scala/com/github/zenpie/macrowave/internal/ids/IdProvider.scala
+++ src/main/scala/com/github/zenpie/macrowave/internal/ids/IdProvider.scala
@@ -1,6 +1,8 @@
 package com.github.zenpie.macrowave.internal.ids
 
-class IdProvider[@specialized T <: Id](f: Int => T) {
+import scala.collection.mutable
+
+final class IdProvider[@specialized T <: Id](f: Int => T) {
 
   private var counter: Int = 0
 
@@ -8,6 +10,28 @@ class IdProvider[@specialized T <: Id](f: Int => T) {
     val current = f(counter)
     counter += 1
     current
+  }
+
+  sealed trait Table[U] {
+    def apply(key: T): U
+    def update(key: T, value: U): Unit
+  }
+
+  def model[U](default: T => U): Table[U] = new Table[U] {
+    private val data = mutable.ArrayBuffer[U]()
+
+    private def ensure(key: T): Int = {
+      val index = key.value
+      while (index >= data.size) data += default(key)
+      index
+    }
+
+    def apply(key: T): U =
+      data(ensure(key))
+
+    def update(key: T, value: U): Unit =
+      data(ensure(key)) = value
+
   }
 
 }

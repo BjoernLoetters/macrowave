3e8edb83df9ffee1e78f7efd89324cb39b16c53c
kuchenkruste <me@kuchenkruste.com>
2017-03-27 16:20:13 +0200

    Moved class `Table` out of class `IdProvider`

--- src/main/scala/com/github/zenpie/macrowave/internal/ids/IdProvider.scala
+++ src/main/scala/com/github/zenpie/macrowave/internal/ids/IdProvider.scala
@@ -2,6 +2,14 @@ package com.github.zenpie.macrowave.internal.ids
 
 import scala.collection.mutable
 
+sealed trait Table[T <: Id, U] {
+
+  def apply(key: T): U
+
+  def update(key: T, value: U): Unit
+
+}
+
 final class IdProvider[T <: Id](f: Int => T) {
 
   private var counter: Int = 0
@@ -12,12 +20,7 @@ final class IdProvider[T <: Id](f: Int => T) {
     current
   }
 
-  sealed trait Table[U] {
-    def apply(key: T): U
-    def update(key: T, value: U): Unit
-  }
-
-  def dataTable[U](default: T => U): Table[U] = new Table[U] {
+  def dataTable[U](default: T => U): Table[T, U] = new Table[T, U] {
     private val data = mutable.ArrayBuffer[U]()
 
     private def ensure(key: T): Int = {
